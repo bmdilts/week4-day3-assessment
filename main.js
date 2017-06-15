1352da44f1521263d307b883cc7605296e1fb1ad
bmdilts <bmdilts@gmail.com>
2017-06-15 09:53:47 -0400

    fixed array within array, it works, yay

--- main.js
+++ main.js
@@ -12,39 +12,47 @@
     })
 
     .then(function(json){
-      console.log(json);
       for(var i = 0; i < 12; i++){
         var flexnum = ("flex_container" + i);
-        console.log(flexnum);
-        console.log(json);
+        var result = json.results;
+
         let flex_container = document.createElement("DIV");
         flex_container.setAttribute("id", flexnum)
-        document.getElementById("customers").appendChild(flex_container);                                                                    // Create a <div> element
+        document.getElementById("customers").appendChild(flex_container);
+                                                                            // Create a <div> element
         let pic = document.createElement("IMG");
-        console.log(json[i]);                           // Create a text node
-        pic.setAttribute("src", json[i].picture.medium);                                                                    // Append the text to <p>
+        pic.setAttribute("src", result[i].picture.medium);                                                                    // Append the text to <p>
         document.getElementById(flexnum).appendChild(pic);           // Append <p> to <div> with id="myDIV"
 
-        let name_div = document.createElement("DIV");                       // Create a <div> element
-        let firstName = document.createTextNode(json[i].name.first)
-        let lastName = document.createTextNode(json[i].name.last);      // Create a text node
+        let name_div = document.createElement("DIV");
+        name_div.setAttribute("class", "name");                       // Create a <div> element
+        let firstName = document.createTextNode(result[i].name.first + " ")
+        let lastName = document.createTextNode(result[i].name.last);      // Create a text node
         name_div.appendChild(firstName);
         name_div.appendChild(lastName);                                          // Append the text to <p>
-        document.getElementById("flex_container" + i).appendChild(name_div);
-        let email = document.createTextNode(json[i].email);
-        document.getElementById("flex_container" + i).appendChild(email);
-        let location_div = document.createElement("DIV");                       // Create a <div> element
-        let street = document.createTextNode(json[i].location.street)
-        let city = document.createTextNode(json[i].location.city);
-        let state = document.createTextNode(json[i].location.state);
-        let postcode = document.createTextNode(json[i].location.postcode);     // Create a text node
+        document.getElementById(flexnum).appendChild(name_div);
+        let email = document.createElement("P")
+        email.textContent = (result[i].email);
+        email.setAttribute("id", "email");
+        document.getElementById(flexnum).appendChild(email);
+        let location_div = document.createElement("DIV");
+        location_div.setAttribute("id", "location");                      // Create a <div> element
+        let street = document.createTextNode(result[i].location.street + " ");
+        let addressLine2 = document.createElement("DIV");
+        let addressLine3 = document.createElement("DIV");
+        let city = document.createTextNode(result[i].location.city + ", ");
+        let state = document.createTextNode(result[i].location.state + " ");
+        let postcode = document.createTextNode(result[i].location.postcode);
         location_div.appendChild(street);
-        location_div.appendChild(city);
-        location_div.appendChild(state);
-        location_div.appendChild(postcode);                                         // Append the text to <p>
-        document.getElementById("flex_container" + i).appendChild(location_div);
-        let value = document.createTextNode(json[i].id.value);
-        document.getElementById("flex_container" + i).appendChild(value);
+        addressLine2.appendChild(city);
+        addressLine2.appendChild(state);
+        addressLine2.appendChild(postcode);
+        location_div.appendChild(addressLine2);                                        // Append the text to <p>
+        document.getElementById(flexnum).appendChild(location_div);
+        let value = document.createElement("P");
+        value.textContent = (result[i].id.value);
+        value.setAttribute("id", "id");
+        document.getElementById(flexnum).appendChild(value);
       }
     });
 
